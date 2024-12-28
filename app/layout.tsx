9b5e0e8f7a68d0bdb4e99998c9dc0ffad9d6f7b0
Alex Haynes <alex@alexhaynes.org>
2024-12-27 23:00:29 -0500

    fix: css 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next'
 import { ThemeProvider } from '../contexts/ThemeContext'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Alex Haynes',
@@ -16,11 +13,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-theme-primary">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/qzy8qpi.css" />
       </head>
-      <body className={inter.className}>
+      <body className="min-h-screen text-theme-text bg-theme-primary">
         <ThemeProvider>
           {children}
         </ThemeProvider>
